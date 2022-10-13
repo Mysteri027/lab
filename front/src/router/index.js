d1c0aafad5b77dc98334a42d7343dc89fa905e5f
Igor <igor.sheplyakov@yandex.ru>
2022-10-13 11:21:59 +0300

    clear vue project
    clear vue project

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,32 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ContactsView from "@/views/ContactsView";
+
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-    {
-        path: '/contacts',
-        name: 'contacts',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: ContactsView
-    }
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
 ]
 
 const router = new VueRouter({
